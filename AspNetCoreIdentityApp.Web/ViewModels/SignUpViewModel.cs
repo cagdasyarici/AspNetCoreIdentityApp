2e3f90db86ee90186cbfc9a8a4c8a7f4b9a5a123
cagdasyarici <cagdasyarici16@gmail.com>
2023-07-23 03:13:41 +0300

    Add project files.

---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel() { }
+        public SignUpViewModel(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+        [Required(ErrorMessage ="Kullanıcı Ad Alanı boş bırakılamaz.")]
+        [Display(Name = "Kullanıcı Adı :")]
+        public string UserName { get; set; }
+        [EmailAddress(ErrorMessage ="Email formatı yanlış.")]
+        [Required(ErrorMessage = "Email Alanı boş bırakılamaz.")]
+        [Display(Name = "Email :")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Telefon Alanı boş bırakılamaz.")]
+        [Display(Name = "Telefon :")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Şifre Alanı boş bırakılamaz.")]
+        [Display(Name = "Şifre :")]
+        public string Password { get; set; }
+        [Compare(nameof(Password),ErrorMessage ="Girmiş olduğunuz şifreler aynı değil.")]
+        [Required(ErrorMessage = "Şifre Tekrar Alanı boş bırakılamaz.")]
+        [Display(Name = "Şifre Tekrar:")]
+        public string PasswordConfirm { get; set; }
+    }
+}
